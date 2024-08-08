766dd1483c30dcaa60bde06f91786dd40ca8c947
ayushRajak19 <ayushnirmal6261@gamail.com>
2024-08-08 13:46:23 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,11 @@
+name = "Annushree"
+print(name[1:3])  #this is callled as slicing 
+print(name[0:5])
+
+
+a = ["karela ", "aalu", 23,56,7]
+print(type(a))
+
+a.append("ayush")
+
+print(a)
---
+++ jokes.py
@@ -0,0 +1,4 @@
+import pyjokes
+# print("Here we are try to you becomes fully llaughter with this jokes ")
+joke = pyjokes.get_joke()
+print(joke)
